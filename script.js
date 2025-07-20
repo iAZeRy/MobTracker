8b6034e614a889f05d9a1c2e9c0500b1e2a9919c
iAZeRy <hlexy@protonmail.com>
2025-07-20 15:56:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,68 @@
-// Funktion zum Laden der Mob-Daten
-fetch('mobs.json')
-  .then(response => response.json())
-  .then(mobs => {
-    const mobListContainer = document.querySelector('.mob-list');
-    mobs.forEach(mob => {
-      const mobItem = document.createElement('div');
-      mobItem.classList.add('mob-item');
-
-      mobItem.innerHTML = `
-        <img src="${mob.image}" alt="${mob.name}">
-        <h2>${mob.name}</h2>
-        <p><strong>Chance:</strong> ${mob.chance}</p>
-        <p><strong>Dimension:</strong> ${mob.dimension}</p>
-        <p><strong>Lichtlevel:</strong> ${mob.light}</p>
-        <p><strong>Bedingungen:</strong> ${mob.conditions.join(', ')}</p>
-        <p><strong>Hinweis:</strong> ${mob.note}</p>
-      `;
-
-      mobListContainer.appendChild(mobItem);
-    });
-  })
-  .catch(error => console.error('Fehler beim Laden der Mobs:', error));
+const mobGroups = [
+  {
+    groupName: "Zombies",
+    mobs: [
+      "Zombie_Blacksmith",
+      "Zombie_Butcher",
+      "Zombie_Farmer",
+      "Zombie_Horse_Revision",
+      "Zombie_in_chainmail_armor",
+      "Zombie_in_diamond_armor",
+      "Zombie_in_golden_armor",
+      "Zombie_in_iron_armor",
+      "Zombie_in_leather_armor",
+      "Zombie_Librarian",
+      "Zombie_Nitwit",
+      "Zombie_Priest",
+      "Zombie_Villager_with_Carved_Pumpkin",
+      "Zombie_with_Carved_Pumpkin"
+    ]
+  },
+  {
+    groupName: "Chicken Jockeys",
+    mobs: [
+      "Chicken_Jockey_with_Carved_Pumpkin",
+      "Chicken_Savanna_Zombie_Jockey",
+      "Chicken_Snowy_Zombie_Jockey",
+      "Chicken_Swamp_Zombie_Jockey",
+      "Chicken_Taiga_Zombie_Jockey",
+      "Chicken_Zombie_Butcher_Jockey",
+      "Chicken_Zombie_Farmer_Jockey",
+      "Chicken_Zombie_Librarian_Jockey",
+      "Chicken_Zombie_Nitwit_Jockey",
+      "Chicken_Zombie_Pigman_Jockey",
+      "Chicken_Zombie_Priest_Jockey",
+      "Chicken_Zombie_Smith_Jockey"
+    ]
+  },
+  {
+    groupName: "Axolotls",
+    mobs: [
+      "Axolotl_Hovering_in_Water_cyan",
+      "Axolotl_Hovering_in_Water_gold"
+    ]
+  },
+  {
+    groupName: "Boss Mobs",
+    mobs: [
+      "Ender_Dragon",
+      "Wither"
+    ]
+  },
+  {
+    groupName: "Hostile Mobs",
+    mobs: [
+      "Aggressive_Panda",
+      "Angry_Wolf",
+      "Drowned",
+      "Enderman",
+      "Wither_Skeleton"
+    ]
+  },
+  {
+    groupName: "Other",
+    mobs: [
+      "Armadillo"
+    ]
+  }
+];
