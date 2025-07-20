d7ad475b88761a71927fe709172d0129e4fe60b5
iAZeRy <hlexy@protonmail.com>
2025-07-20 23:08:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ searchInput.addEventListener('input', (e) => {
 });
 
 function showInfoPanel(mob) {
-    openModal(`<h2>${mob.name}</h2><p>${mob.description || "Keine Beschreibung."}</h3></p>${mob.spawnrate || "Keine Spawnrate."}</p>`);
+    openModal(`<h2>${mob.name}</h2><p>${mob.description || "Keine Beschreibung."}</h2></p>${mob.spawnrate || "Keine Spawnrate."}</p>`);
 }
 
 let allMobs = [];
